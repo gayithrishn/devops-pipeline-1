a0715b5a2be3549663a554f007c8ea261cf86b9e
gayithrishn <gayithrishn@gmail.com>
2025-04-01 15:47:22 +0530

    first updated code

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,18 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int num1=20 ;
+        int num2=30;
+        int sum=num1+num2;
+        System.out.println("20+30 = " + sum);
+
+        System.out.println("Application executed sucessfully!");
+    }
+}
